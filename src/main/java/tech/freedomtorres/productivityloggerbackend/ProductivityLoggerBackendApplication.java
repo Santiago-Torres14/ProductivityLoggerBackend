c93a4bae1780a0ad4131036204fcf691ac525e9a
Santiago Torres Aragon <santiagoatorres14@gmail.com>
2022-09-21 13:31:29 -0500

    Scaffolding project and database configuration

---
+++ src/main/java/tech/freedomtorres/productivityloggerbackend/ProductivityLoggerBackendApplication.java
@@ -0,0 +1,13 @@
+package tech.freedomtorres.productivityloggerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductivityLoggerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductivityLoggerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/freedomtorres/productivityloggerbackend/ProductivityLoggerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.freedomtorres.productivityloggerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductivityLoggerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
